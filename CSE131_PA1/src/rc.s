31ca9c09bbb25147aa38d96f5b7d262d567a8ad5
davidweinfls <davidweinfls@163.com>
2014-05-25 21:30:13 -0700

    5/25 test version

--- CSE131_PA1/src/rc.s
+++ CSE131_PA1/src/rc.s
@@ -1,5 +1,5 @@
 ! 
-! Generated Sun May 25 21:11:37 PDT 2014
+! Generated Sun May 25 21:29:47 PDT 2014
 ! 
 
 	.section ".rodata"
@@ -11,98 +11,28 @@
 .NullPtrException:	.asciz "Cannot dereference a nullPointer.\n"
 	.align 4
 
+temp0:	.asciz "baz's sword: "
+	.align 4
 	.section ".data"
 	.align 4
 
-	.global x
+	.global baz
 	
 	.section ".bss"
 	.align 4
 
-x:	.skip 4
+	.global magic
+magic:	.skip 4
+	
+baz:	.skip 4
 
 	.section ".text"
 	.align 4
 
 
+! --------in writeGlobalStruct--------
+
 ! ---------In writeGLobalVariable--------------
-
-! in writeFuncDec
-! <<<<<<<<<<<<<<<<<<<foo>>>>>>>>>>>>>>>>>
-	.align 4
-	.global foo
-foo:
-	set	SAVE.foo, %g1
-	save	%sp, %g1, %sp
-
-
-! -------in writeParameter: a param num: 0
-	set	68, %l0
-	add	%fp, %l0, %l0
-	st	%i0, [%l0]
-
-! ------in writeConstantLiteral: 3
-	set	3, %l1
-	set	-4, %l0
-	add	%fp, %l0, %l0
-	st	%l1, [%l0]
-
-! ------end of writeConstantLiteral-------
-
-! ----------in writeAssignExpr: a  =  3
-
-! -------in getValue: 3: 3.0
-
-! --------in getAddressHelper: 3
-	set	-4, %l0
-	add	%fp, %l0, %l0
-
-! --------end of getAddressHelper------------ 
-	ld	[%l0], %l1
-
-! -------end of getValue------------
-
-! --------in getAddressHelper: a
-	set	68, %l0
-	add	%fp, %l0, %l0
-
-! --------end of getAddressHelper------------ 
-	ld	[%l0], %l0
-	st	%l1, [%l0]
-
-! ----------end of writeAssignExpr--------
-
-! ------------in writePrint---------------
-
-! -------in getValue: a: null
-
-! --------in getAddressHelper: a
-	set	68, %l0
-	add	%fp, %l0, %l0
-
-! --------end of getAddressHelper------------ 
-	ld	[%l0], %l0
-	ld	[%l0], %l1
-
-! -------end of getValue------------
-	set	.intFmt, %o0
-	mov	%l1, %o1
-	call	printf
-	nop
-
-
-! ------------in writePrint---------------
-	set	.endl, %o0
-	call	printf
-	nop
-
-
-! --------------in writeFuncClose--------------
-	ret
-	restore
-
-	SAVE.foo = -(92 + 4) & -8
-
 
 ! in writeFuncDec
 ! <<<<<<<<<<<<<<<<<<<main>>>>>>>>>>>>>>>>>
@@ -113,33 +43,89 @@ main:
 	save	%sp, %g1, %sp
 
 
-! -------in writePassParameter--------
+! ------in writeConstantLiteral: 54
+	set	54, %l1
+	set	-4, %l0
+	add	%fp, %l0, %l0
+	st	%l1, [%l0]
 
-! =====in writePassParameter, param x is byRef=======
+! ------end of writeConstantLiteral-------
 
-! --------in getAddressHelper: x
-	set	x, %l0
+! ----------in writeAssignExpr: a  =  54
+
+! -------in getValue: 54: 54.0
+
+! --------in getAddressHelper: 54
+	set	-4, %l0
+	add	%fp, %l0, %l0
+
+! --------end of getAddressHelper------------ 
+	ld	[%l0], %l1
+
+! -------end of getValue------------
+
+! --------in getAddressHelper: a
+
+! -------in writeStructAddress: a
+	set	magic, %l0
+	add	%g0, %l0, %l0
+	add	%l0, 0, %l0
+
+! --------end of getAddressHelper------------ 
+	st	%l1, [%l0]
+
+! ----------end of writeAssignExpr--------
+
+! ------in writeAddressOf: magic
+
+! --------in getAddressHelper: magic
+	set	magic, %l0
 	add	%g0, %l0, %l0
 
 ! --------end of getAddressHelper------------ 
-	mov	%l0, %o0
+	mov	%l0, %l1
+	set	-8, %l0
+	add	%fp, %l0, %l0
+	st	%l1, [%l0]
 
-! -------end of writePassParameter--------
+! -------end of writeAddressOf-------
 
-! ----------writeFuncCall------------
-	call	foo
+! ----------in writeAssignExpr: baz  =  magic
+
+! -------in getValue: magic: null
+
+! --------in getAddressHelper: magic
+	set	-8, %l0
+	add	%fp, %l0, %l0
+
+! --------end of getAddressHelper------------ 
+	ld	[%l0], %l1
+
+! -------end of getValue------------
+
+! --------in getAddressHelper: baz
+	set	baz, %l0
+	add	%g0, %l0, %l0
+
+! --------end of getAddressHelper------------ 
+	st	%l1, [%l0]
+
+! ----------end of writeAssignExpr--------
+
+! ------------in writePrint---------------
+	set	temp0, %o0
+	call	printf
 	nop
 
 
-! ========writeFuncCall: get address of retSTO, store retValue in it ==========
-
 ! ------------in writePrint---------------
 
-! -------in getValue: x: null
+! -------in getValue: a: null
 
-! --------in getAddressHelper: x
-	set	x, %l0
-	add	%g0, %l0, %l0
+! --------in getAddressHelper: a
+
+! -------in writeStructAddress: a
+	add	%l0, 0, %l0
 
 ! --------end of getAddressHelper------------ 
 	ld	[%l0], %l1
@@ -161,5 +147,5 @@ main:
 	ret
 	restore
 
-	SAVE.main = -(92 + 4) & -8
+	SAVE.main = -(92 + 8) & -8
 
