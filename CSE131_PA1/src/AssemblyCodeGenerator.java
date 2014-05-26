35878d3078a984109bc8d11114fbe19186dc6f8d
davidweinfls <davidweinfls@163.com>
2014-05-25 21:12:11 -0700

    5/25 fixed struct byref param bug

--- CSE131_PA1/src/AssemblyCodeGenerator.java
+++ CSE131_PA1/src/AssemblyCodeGenerator.java
@@ -514,7 +514,7 @@ public class AssemblyCodeGenerator {
     	{
     		
     	}
-    	else if(sto.getStruct().getType().isPointerType() || (sto.isVar() && ((VarSTO)sto).isRef() ))
+    	else if(sto.getStruct().getType().isPointerType() || (sto.getStruct().isVar() && ((VarSTO)sto.getStruct()).isRef() ))
     	{
     		if(debug) writeDebug("=======in writeStructAddress: " + sto.getStruct().getName() + " is a ptr or byRef========");
 
--- CSE131_PA1/src/rc.s
+++ CSE131_PA1/src/rc.s
@@ -1,5 +1,5 @@
 ! 
-! Generated Sun May 25 17:03:41 PDT 2014
+! Generated Sun May 25 21:11:37 PDT 2014
 ! 
 
 	.section ".rodata"
@@ -11,25 +11,15 @@
 .NullPtrException:	.asciz "Cannot dereference a nullPointer.\n"
 	.align 4
 
-temp0:	.asciz "i is: "
-	.align 4
-temp1:	.asciz "value of ip is: "
-	.align 4
-temp2:	.asciz "value of ip is: "
-	.align 4
 	.section ".data"
 	.align 4
 
-	.global i
-	
-	.global j
+	.global x
 	
 	.section ".bss"
 	.align 4
 
-i:	.skip 4
-
-j:	.skip 4
+x:	.skip 4
 
 	.section ".text"
 	.align 4
@@ -37,36 +27,34 @@ j:	.skip 4
 
 ! ---------In writeGLobalVariable--------------
 
-! ---------In writeGLobalVariable--------------
-
 ! in writeFuncDec
-! <<<<<<<<<<<<<<<<<<<foo1>>>>>>>>>>>>>>>>>
+! <<<<<<<<<<<<<<<<<<<foo>>>>>>>>>>>>>>>>>
 	.align 4
-	.global foo1
-foo1:
-	set	SAVE.foo1, %g1
+	.global foo
+foo:
+	set	SAVE.foo, %g1
 	save	%sp, %g1, %sp
 
 
-! -------in writeParameter: p param num: 0
+! -------in writeParameter: a param num: 0
 	set	68, %l0
 	add	%fp, %l0, %l0
 	st	%i0, [%l0]
 
-! ------in writeConstantLiteral: 5
-	set	5, %l1
-	set	-8, %l0
+! ------in writeConstantLiteral: 3
+	set	3, %l1
+	set	-4, %l0
 	add	%fp, %l0, %l0
 	st	%l1, [%l0]
 
 ! ------end of writeConstantLiteral-------
 
-! ----------in writeAssignExpr: p  =  5
+! ----------in writeAssignExpr: a  =  3
 
-! -------in getValue: 5: 5.0
+! -------in getValue: 3: 3.0
 
-! --------in getAddressHelper: 5
-	set	-8, %l0
+! --------in getAddressHelper: 3
+	set	-4, %l0
 	add	%fp, %l0, %l0
 
 ! --------end of getAddressHelper------------ 
@@ -74,83 +62,46 @@ foo1:
 
 ! -------end of getValue------------
 
-! --------in getAddressHelper: p
-
-! -------in writeDerefAddress: p
-
-! --------in getAddressHelper: p
+! --------in getAddressHelper: a
 	set	68, %l0
 	add	%fp, %l0, %l0
 
 ! --------end of getAddressHelper------------ 
 	ld	[%l0], %l0
-
-! ======in writeDerefAddress, check nullPtrExcep=======
-	set	0, %l4
-	cmp	%l0, %l4
-	bne	ptrLabel0
-	nop
-
-	set	.NullPtrException, %o0
-	call	printf
-	nop
-
-	set	1, %o0
-	call	exit
-	nop
-
-ptrLabel0:
-
-! ======end of check nullPtrExcep=======
-
-! -------end of writeDerefAddress-------
-
-! --------end of getAddressHelper------------ 
 	st	%l1, [%l0]
 
 ! ----------end of writeAssignExpr--------
 
-! ------in writeAddressOf: j
+! ------------in writePrint---------------
 
-! --------in getAddressHelper: j
-	set	j, %l0
-	add	%g0, %l0, %l0
+! -------in getValue: a: null
 
-! --------end of getAddressHelper------------ 
-	mov	%l0, %l1
-	set	-12, %l0
-	add	%fp, %l0, %l0
-	st	%l1, [%l0]
-
-! -------end of writeAddressOf-------
-
-! ----------in writeAssignExpr: p  =  j
-
-! -------in getValue: j: null
-
-! --------in getAddressHelper: j
-	set	-12, %l0
-	add	%fp, %l0, %l0
-
-! --------end of getAddressHelper------------ 
-	ld	[%l0], %l1
-
-! -------end of getValue------------
-
-! --------in getAddressHelper: p
+! --------in getAddressHelper: a
 	set	68, %l0
 	add	%fp, %l0, %l0
 
 ! --------end of getAddressHelper------------ 
-	st	%l1, [%l0]
+	ld	[%l0], %l0
+	ld	[%l0], %l1
 
-! ----------end of writeAssignExpr--------
+! -------end of getValue------------
+	set	.intFmt, %o0
+	mov	%l1, %o1
+	call	printf
+	nop
+
+
+! ------------in writePrint---------------
+	set	.endl, %o0
+	call	printf
+	nop
+
 
 ! --------------in writeFuncClose--------------
 	ret
 	restore
 
-	SAVE.foo1 = -(92 + 12) & -8
+	SAVE.foo = -(92 + 4) & -8
 
 
 ! in writeFuncDec
@@ -162,209 +113,33 @@ main:
 	save	%sp, %g1, %sp
 
 
-! ------in writeAddressOf: i
+! -------in writePassParameter--------
 
-! --------in getAddressHelper: i
-	set	i, %l0
+! =====in writePassParameter, param x is byRef=======
+
+! --------in getAddressHelper: x
+	set	x, %l0
 	add	%g0, %l0, %l0
 
 ! --------end of getAddressHelper------------ 
-	mov	%l0, %l1
-	set	-16, %l0
-	add	%fp, %l0, %l0
-	st	%l1, [%l0]
-
-! -------end of writeAddressOf-------
-
-! -------in getValue: i: null
-
-! --------in getAddressHelper: i
-	set	-16, %l0
-	add	%fp, %l0, %l0
-
-! --------end of getAddressHelper------------ 
-	ld	[%l0], %l1
-
-! -------end of getValue------------
-
-! ---------in writeLocalVariableWInit:ip
-	set	-20, %l0
-	add	%fp, %l0, %l0
-	st	%l1, [%l0]
-
-
-! -------in writePassParameter--------
-	set	-20, %l0
-	add	%fp, %l0, %l0
-	ld	[%l0], %l1
-	mov	%l1, %o0
+	mov	%l0, %o0
 
 ! -------end of writePassParameter--------
 
 ! ----------writeFuncCall------------
-	call	foo1
+	call	foo
 	nop
 
 
 ! ========writeFuncCall: get address of retSTO, store retValue in it ==========
 
 ! ------------in writePrint---------------
-	set	temp0, %o0
-	call	printf
-	nop
 
+! -------in getValue: x: null
 
-! ------------in writePrint---------------
-
-! -------in getValue: i: null
-
-! --------in getAddressHelper: i
-	set	i, %l0
+! --------in getAddressHelper: x
+	set	x, %l0
 	add	%g0, %l0, %l0
-
-! --------end of getAddressHelper------------ 
-	ld	[%l0], %l1
-
-! -------end of getValue------------
-	set	.intFmt, %o0
-	mov	%l1, %o1
-	call	printf
-	nop
-
-
-! ------------in writePrint---------------
-	set	.endl, %o0
-	call	printf
-	nop
-
-
-! ------------in writePrint---------------
-	set	temp1, %o0
-	call	printf
-	nop
-
-
-! ------------in writePrint---------------
-
-! -------in getValue: ip: null
-
-! --------in getAddressHelper: ip
-
-! -------in writeDerefAddress: ip
-
-! --------in getAddressHelper: ip
-	set	-20, %l0
-	add	%fp, %l0, %l0
-
-! --------end of getAddressHelper------------ 
-	ld	[%l0], %l0
-
-! ======in writeDerefAddress, check nullPtrExcep=======
-	set	0, %l4
-	cmp	%l0, %l4
-	bne	ptrLabel1
-	nop
-
-	set	.NullPtrException, %o0
-	call	printf
-	nop
-
-	set	1, %o0
-	call	exit
-	nop
-
-ptrLabel1:
-
-! ======end of check nullPtrExcep=======
-
-! -------end of writeDerefAddress-------
-
-! --------end of getAddressHelper------------ 
-	ld	[%l0], %l1
-
-! -------end of getValue------------
-	set	.intFmt, %o0
-	mov	%l1, %o1
-	call	printf
-	nop
-
-
-! ------------in writePrint---------------
-	set	.endl, %o0
-	call	printf
-	nop
-
-
-! ------in writeConstantLiteral: 99
-	set	99, %l1
-	set	-28, %l0
-	add	%fp, %l0, %l0
-	st	%l1, [%l0]
-
-! ------end of writeConstantLiteral-------
-
-! ----------in writeAssignExpr: j  =  99
-
-! -------in getValue: 99: 99.0
-
-! --------in getAddressHelper: 99
-	set	-28, %l0
-	add	%fp, %l0, %l0
-
-! --------end of getAddressHelper------------ 
-	ld	[%l0], %l1
-
-! -------end of getValue------------
-
-! --------in getAddressHelper: j
-	set	j, %l0
-	add	%g0, %l0, %l0
-
-! --------end of getAddressHelper------------ 
-	st	%l1, [%l0]
-
-! ----------end of writeAssignExpr--------
-
-! ------------in writePrint---------------
-	set	temp2, %o0
-	call	printf
-	nop
-
-
-! ------------in writePrint---------------
-
-! -------in getValue: ip: null
-
-! --------in getAddressHelper: ip
-
-! -------in writeDerefAddress: ip
-
-! --------in getAddressHelper: ip
-	set	-20, %l0
-	add	%fp, %l0, %l0
-
-! --------end of getAddressHelper------------ 
-	ld	[%l0], %l0
-
-! ======in writeDerefAddress, check nullPtrExcep=======
-	set	0, %l4
-	cmp	%l0, %l4
-	bne	ptrLabel2
-	nop
-
-	set	.NullPtrException, %o0
-	call	printf
-	nop
-
-	set	1, %o0
-	call	exit
-	nop
-
-ptrLabel2:
-
-! ======end of check nullPtrExcep=======
-
-! -------end of writeDerefAddress-------
 
 ! --------end of getAddressHelper------------ 
 	ld	[%l0], %l1
@@ -386,5 +161,5 @@ ptrLabel2:
 	ret
 	restore
 
-	SAVE.main = -(92 + 32) & -8
+	SAVE.main = -(92 + 4) & -8
 
